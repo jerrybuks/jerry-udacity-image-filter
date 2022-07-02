2a41d6c04f11741e7cd7313bd1c39c27ee989c69
Chibuokem Jerry <profkiti@gmail.com>
2022-07-02 10:14:32 +0100

    completed project 2

--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import Jimp = require("jimp");
+const axios = require("axios").default;
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -11,14 +12,20 @@ import Jimp = require("jimp");
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
+      const res = await axios({
+        method: "get",
+        url: inputURL,
+        responseType: "arraybuffer",
+      });
+
+      const photo = await Jimp.read(res.data);
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
         .greyscale() // set greyscale
-        .write(__dirname + outpath, (img) => {
+        .write(__dirname + outpath, () => {
           resolve(__dirname + outpath);
         });
     } catch (error) {
